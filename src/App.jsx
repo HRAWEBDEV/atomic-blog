52bf55fdca81916ab009518c85d0b7d55e5bddb5
hamidwise1 <hrawebdev@gmail.com>
2023-09-21 19:06:11 +0430

    add mode provider to app file

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Footer from './components/Footer';
 import Archive from './components/Archive';
 import Main from './components/Main';
 import ModeController from './components/ModeController';
+import ModeProvider from './components/ModeProvider';
 
 function App() {
  return (
-  <PostsProvider>
-   <section>
-    <ModeController />
-    <Header />
-    <Main />
-    <Archive />
-    <Footer />
-   </section>
-  </PostsProvider>
+  <ModeProvider>
+   <PostsProvider>
+    <section>
+     <ModeController />
+     <Header />
+     <Main />
+     <Archive />
+     <Footer />
+    </section>
+   </PostsProvider>
+  </ModeProvider>
  );
 }
 
